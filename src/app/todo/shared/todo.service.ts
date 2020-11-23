154e3752501e6306d113d584342480edeb4b2ee9
benjdia <cravatas90@gmail.com>
2020-11-23 16:27:10 +0100

    Création des fonctions pour l'ajout, suppression

--- src/app/todo/shared/todo.service.ts
+++ src/app/todo/shared/todo.service.ts
@@ -7,8 +7,25 @@ import { AngularFireDatabase, AngularFireList } from 'angularfire2/database'
 export class TodoService {
   toDoList: AngularFireList<any>;
   constructor(private firebasedb: AngularFireDatabase) { }
+  
   getToDoList() {
     this.toDoList = this.firebasedb.list('titles');
     return this.toDoList;
   }
+
+  addTitle(title: string) {
+    this.toDoList.push({
+      title: title,
+      isChecked: false
+    });
+  }
+
+  checkOrUnCheckTitle($key: string, flag: boolean) {
+    this.toDoList.update($key, { isChecked: flag });
+  }
+
+  removeTitle($key: string) {
+    this.toDoList.remove($key);
+  }
+  
 }
