a17d9c117f441355adbe084a910e5e29e7ff49d3
benjdia <cravatas90@gmail.com>
2020-11-23 16:44:16 +0100

    création d'un component "todo"

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { TodoService } from './shared/todo.service';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.css']
+  styleUrls: ['./todo.component.css'],
+  providers : [TodoService]
 })
 export class TodoComponent implements OnInit {
+  
+  toDoListArray: any[];
+  constructor(private toDoService: TodoService) { }
 
-  constructor() { }
+  ngOnInit() {
+    this.toDoService.getToDoList().snapshotChanges()
+    .subscribe(item => {
+      this.toDoListArray = [];
+      item.forEach(element => {
+        var x = element.payload.toJSON();
+        x["$key"] = element.key;
+        this.toDoListArray.push(x);
+      })
 
-  ngOnInit(): void {
+      //sort array isChecked false  -> true
+        this.toDoListArray.sort((a,b) => {
+          return a.isChecked - b.isChecked;
+        })
+    });
   }
 
 }
